255b0104986d8d1b88b86d4afb6071390b2ff6b7
lemmonmark <lemmonmark@c67ee986-0855-0410-825f-15918b819f62>
2008-08-18 03:36:21 +0000

    added methods

--- trunk/JukeBox/src/playlist/PlaylistObserver.java
+++ trunk/JukeBox/src/playlist/PlaylistObserver.java
@@ -2,6 +2,16 @@ package playlist;
 
 public interface PlaylistObserver {
 	
+	public void playListUpdated(Playlist p);
 
+	public void trackEnded();
+
+	public void trackPaused();
+
+	public void trackPlayed();
+
+	public void trackStarted(Track t);
+
+	public void volumeChanged(int newVolume);
 
 }
