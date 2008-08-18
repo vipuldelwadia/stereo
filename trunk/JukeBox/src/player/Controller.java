0faee87258087b2eee88045dc6ba5186cd414590
lemmonmark <lemmonmark@c67ee986-0855-0410-825f-15918b819f62>
2008-08-18 04:17:15 +0000

    controller stubs tested

--- trunk/JukeBox/src/player/Controller.java
+++ trunk/JukeBox/src/player/Controller.java
@@ -1,44 +1,39 @@
 package player;
 
-import playlist.Playlist;
-import playlist.PlaylistObserver;
-import playlist.Track;
 
 /**
  * 
  * @author coxdyla
  *This class interfaces the User interfaces with the servers playlist
  */
-public class Controller implements PlaylistObserver{
-
-	public void playListUpdated(Playlist p) {
-		// TODO Auto-generated method stub
-		
+public class Controller {
+	
+	/**
+	 * pauses the playing track
+	 *
+	 */
+	public void pauseTrack() {
+		// TODO Auto-generated method stub (ha!)
+		System.out.println("pause track");
 	}
 
-	public void trackEnded() {
+	/**
+	 * plays the paused track
+	 */
+	public void playTrack() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("play track");
 	}
 
-	public void trackPaused() {
+	/**
+	 * change the volume to the stated value
+	 * @param newVolume int between 0 and 10
+	 * @throws IllegalArgumentException if newVolume is < 0 or > 10
+	 */
+	public void changeVolume(int newVolume) {
+		if (newVolume < 0 || newVolume > 10) throw new IllegalArgumentException("volume must be between 0-10");
 		// TODO Auto-generated method stub
-		
-	}
-
-	public void trackPlayed() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void trackStarted(Track t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void volumeChanged(int newVolume) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("change volume: " + newVolume);
 	}
 
 }
--- trunk/JukeBox/test/controller/ControllerTest.java
+++ trunk/JukeBox/test/controller/ControllerTest.java
@@ -1,6 +1,44 @@
 package controller;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import player.Controller;
+
 
 public class ControllerTest {
+	Controller c;
 
+	@Before
+	public void testInstantiation(){
+		c = new Controller();
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void volumeSetNegative(){
+		c.changeVolume(-1);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void volumeSetGreaterThanTen(){
+		c.changeVolume(11);
+	}
+	
+	@Test
+	public void volumeAcceptableValue(){
+		c.changeVolume(0);
+		c.changeVolume(4);
+		c.changeVolume(10);
+	}
+	
+	@Test
+	public void trackPlayed() {
+		c.playTrack();
+	}
+	
+	@Test
+	public void trackPaused() {
+		c.pauseTrack();
+	}
+	
 }
