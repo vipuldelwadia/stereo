e6efa6ab30b46059e2084fa4e29250fc6e01e20b
coxdyla <coxdyla@c67ee986-0855-0410-825f-15918b819f62>
2008-08-18 04:38:37 +0000

    testing and implementing CLI

--- trunk/JukeBox/src/interfaces/CLI.java
+++ trunk/JukeBox/src/interfaces/CLI.java
@@ -4,16 +4,18 @@ import java.util.List;
 import java.util.Scanner;
 
 import player.Controller;
+import playlist.PlaylistObserver;
 import playlist.Track;
 
 
 
-public class CLI {
-	private Controller c;
+public class CLI{
 	private Scanner scan;
+	private Controller controller;
 	
 	public CLI(){
 		scan = new Scanner(System.in);
+		controller = Controller.getInstance();
 	}
 	
 	public void run(){
@@ -31,73 +33,32 @@ public class CLI {
 		
 	public void input(String input){
 		if(input.equalsIgnoreCase("Play")){
-			try {
-				c.play();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Playing failed");
-				e.printStackTrace();
-			}
+			controller.playTrack();
 			//TODO send play command 
 		}
 		else if(input.equalsIgnoreCase("Pause")){
-			try {
-				c.pause();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("Pausing failed");
-				e.printStackTrace();
-			}
+			controller.pauseTrack();
 			//TODO send pause command
 		}
 		else if(input.equalsIgnoreCase("skip")){
-			try {
-				c.skip();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("skipping failed");
-				e.printStackTrace();
-			}
 			//TODO send skip command
 		}
 		else if(input.startsWith("set volume ")){
 			input = input.substring(11);
 			int vol = Integer.parseInt(input);
-			c.setVolume(vol);
-			System.out.println("Volume set to " + vol);
+			controller.changeVolume(vol);
+			
 		}
 		else if(input.equalsIgnoreCase("get volume ")){
-			System.out.println("Volume is: " + c.getVolume());
+			//TODO get volume
 		}
 		else if(input.startsWith("playlist ")){
 			input = input.substring(9);
 			int songs = Integer.parseInt(input);
-			List<Track> playlist = c.getList(songs);
-			for(Track s: playlist){
-				System.out.println(s);
-			}
-		}
-		else if(input.equalsIgnoreCase("back")){
-			try {
-				c.back();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				System.out.println("changing Track failed");
-				e.printStackTrace();
-			}
+			//TODO Pass the message on
 		}
 		else if(input.equalsIgnoreCase("details")){
-				try {
-					Track Track = c.getCurrentSong();
-					System.out.println("Track: " + Track.getTitle() );
-					System.out.println("Artist: " + Track.getArtist() );
-					System.out.println("Album: " + Track.getAlbum() );
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					System.out.println("Track details cannot be returned");
-					e.printStackTrace();
-				}
-			//TODO send previous sohng command
+			//TODO get song details printed
 		}
 	}
 
