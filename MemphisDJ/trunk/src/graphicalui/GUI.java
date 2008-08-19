4477747d5a3b3173cbc69ba4f6c22552322966a7
Joris Coppieters <Joris Coppieters@c67ee986-0855-0410-825f-15918b819f62>
2008-08-19 21:29:11 +0000

    Interface

---
+++ MemphisDJ/trunk/src/graphicalui/GUI.java
@@ -0,0 +1,13 @@
+package graphicalui;
+
+public class GUI {
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
