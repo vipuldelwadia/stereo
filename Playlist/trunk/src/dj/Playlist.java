0bcfe6480ed947c45bb7d0bb8dcc794441179a7f
abrahajoav <abrahajoav@c67ee986-0855-0410-825f-15918b819f62>
2008-08-20 23:00:52 +0000

    Initial import.

---
+++ Playlist/trunk/src/dj/Playlist.java
@@ -0,0 +1,53 @@
+package dj;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Playlist implements Iterable<Track> {
+	
+	private ArrayList<Track> tracks;
+
+	public Playlist(){
+		tracks = new ArrayList<Track>();
+	}
+	
+	public boolean addTrack(Track t){
+		return tracks.add(t);
+	}
+	
+	public boolean removeTrack(Track t){
+		return tracks.remove(t);
+	}
+
+	public Iterator<Track> iterator(){
+		return tracks.iterator();
+	}
+	
+	public boolean contains(Track t){
+		if(tracks.contains(t)){
+			return true;
+		}
+		return false;
+	}
+	
+	public int size(){
+		return tracks.size();
+	}
+	
+	public Track peek(){
+		return tracks.get(0);
+	}
+	
+	public Track poll(){
+		Track t = tracks.get(0);
+		removeTrack(t);
+		return t;
+	}
+
+	public boolean isEmpty() {
+		return tracks.isEmpty();
+	}
+
+	
+}
