7d525b2380136f6d29b8a841ec9f4e0752c90777
Joris Coppieters <Joris Coppieters@c67ee986-0855-0410-825f-15918b819f62>
2008-08-20 04:44:32 +0000

    Attempt at parser.

---
+++ DACPLib/src/daccpclient/DACPEntry.java
@@ -0,0 +1,24 @@
+package daccpclient;
+
+import java.math.BigInteger;
+
+public class DACPEntry {
+    
+    private int name;
+    private int datalength;
+    
+    public DACPEntry(int _name, int _datalength){
+        this.name = _name;
+        this.datalength = _datalength;
+    }
+    
+    public String getName(){
+        
+        BigInteger i = BigInteger.valueOf(name);
+        i.byteValue();
+        
+        return "";
+        
+    }
+    
+}
