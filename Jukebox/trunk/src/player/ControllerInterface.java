56a7f8f4d168ee1cffa2448a4aadd7ddc783ba3f
abrahajoav <abrahajoav@c67ee986-0855-0410-825f-15918b819f62>
2008-08-20 23:30:20 +0000

    Sorry, forgot a new file! ^__^

---
+++ Jukebox/trunk/src/player/ControllerInterface.java
@@ -0,0 +1,44 @@
+package player;
+
+import jukebox.Playlist;
+
+
+
+public interface ControllerInterface {
+
+	public abstract boolean isValidController();
+
+	/**
+	 * pauses the playing track
+	 * 
+	 */
+	public abstract void pauseTrack();
+
+	/**
+	 * plays the paused track
+	 */
+	public abstract void playTrack();
+	
+	public abstract void setPlaylist();
+
+	public abstract Playlist getPlaylist();
+
+	/**
+	 * change the volume to the stated value
+	 * 
+	 * @param newVolume
+	 *            int between 0 and 10
+	 * @throws IllegalArgumentException
+	 *             if newVolume is < 0 or > 10
+	 */
+	public abstract void changeVolume(int newVolume);
+
+	/**
+	 * skips to the next track
+	 * 
+	 */
+	public abstract void skipTrack();
+
+	public abstract int getVolume();
+
+}
