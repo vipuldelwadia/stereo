34f732780f93c9c24465429e601cb885095390f3
oneilwill <oneilwill@c67ee986-0855-0410-825f-15918b819f62>
2008-08-21 22:23:48 +0000

    Made track work with constants file.

--- DAAPLib/trunk/src/playlist/Track.java
+++ DAAPLib/trunk/src/playlist/Track.java
@@ -6,19 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import daap.DAAPClient;
-import daap.DAAPUtilities;
+import daap.DAAPConstants;
 
 public class Track {
-	private static final int TRACKID = DAAPUtilities.stringToInt("miid");
-	public static final int ALBUM = DAAPUtilities.stringToInt("asal");
-	public static final int ARTIST = DAAPUtilities.stringToInt("asar");
-	public static final int BITRATE = DAAPUtilities.stringToInt("asbr");
-	public static final int COMPOSER = DAAPUtilities.stringToInt("ascp");
-	public static final int GENRE = DAAPUtilities.stringToInt("asgn");
-	public static final int NAME = DAAPUtilities.stringToInt("minm");
-	public static final int TIME = DAAPUtilities.stringToInt("astm");
-	public static final int START_TIME = DAAPUtilities.stringToInt("asst");
-	public static final int STOP_TIME = DAAPUtilities.stringToInt("assp");
 	
 	private Map<Integer, Object> tags;
 	private DAAPClient publisher;
@@ -46,7 +36,7 @@ public class Track {
 	}
 
 	public int getTrackId() {
-		return (Integer)tags.get(TRACKID);
+		return (Integer)tags.get(DAAPConstants.TRACK_ID);
 	}
 	
 	public DAAPClient getParent(){
@@ -59,7 +49,7 @@ public class Track {
 	}
 	
 	public String toString(){
-		 return String.format("%-25s %-25s %-25s",getTag(Track.ARTIST),getTag(Track.ALBUM),getTag(Track.NAME));
+		 return String.format("%-25s %-25s %-25s",getTag(DAAPConstants.ARTIST),getTag(DAAPConstants.ALBUM),getTag(DAAPConstants.ITEM_NAME));
 	}
 	
 }
