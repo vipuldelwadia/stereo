12973d1d54f978d5e48b7e87cdcee9c8d83f5b47
oneilwill <oneilwill@c67ee986-0855-0410-825f-15918b819f62>
2008-08-21 23:51:42 +0000

    Updated CLI.

--- Jukebox/trunk/src/interfaces/CLI.java
+++ Jukebox/trunk/src/interfaces/CLI.java
@@ -4,11 +4,9 @@ import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Scanner;
 
-import controller.ControllerInterface;
-
-
 import player.Controller;
 import playlist.Track;
+import controller.ControllerInterface;
 
 public class CLI {
     private Scanner    scan;
@@ -20,6 +18,12 @@ public class CLI {
         scan = new Scanner(System.in);
         this.controller = controller;
         run();
+    }
+    
+    public CLI(ControllerInterface controller, String args) {
+        scan = new Scanner(System.in);
+        this.controller = controller;
+        input(args);
     }
     
     public void run() {
@@ -35,26 +39,56 @@ public class CLI {
     }
     
     private class Top {
-        public void list() {
-        	List<Track> p = controller.getPlaylist();
+        public void list(String dummy) {
+            List<Track> p = controller.getPlaylist();
             for(Track t:p){
                 System.out.println(t.toString());
             }
         }
-        public void play() {
+        public void play(String dummy) {
             controller.playTrack();
+        	status(null);
         }
-        public void pause() {
+        
+        public void status(String dummy){
+        	controller.status();
+        }
+        
+        public void filter(String param) {
+        	//TODO FIX THIS HORRIBLE THING
+        	//s=s.replace("_", " ");
+        	//String crit = "";
+        	System.out.println(param);
+        	Scanner s = new Scanner(param);
+        	String type,crit;
+        	type=s.hasNext()? s.next().trim(): "";
+        	crit=s.hasNextLine()? s.nextLine().trim():"";
+        	System.out.println("Type: "+type+"|"+crit+"|");
+            controller.filter(type, crit);
+            status(null);
+        }
+        
+        public void tracklist(String dummy) {
+        	System.out.println(controller.getPlaylist().toString());
+        }
+        
+        public void pause(String dummy) { 
             controller.pauseTrack();
+            status(null);
         }
-        public void skip() {
+        public void skip(String dummy) {
             controller.skipTrack();
+            status(null);
         }
-        public Object set() {
+        public void set(String command) {
+        	command = command.toLowerCase().trim();
+        	if (command.startsWith("volume ")) {
+        		new Set().volume(command.substring("volume ".length()));
+        	}
             System.out.println("set");
-            return new Set();
         }
-        public void stop(){
+        public void stop(String dummy){
+        	System.out.println("Stopped");
         	controller.stop();
         }
     }
@@ -76,7 +110,6 @@ public class CLI {
         Scanner sc = new Scanner(input);
         
         Object o = new Top();
-        
         while (sc.hasNext()) {
             try {
                 Method[] methods = o.getClass().getMethods();
@@ -85,17 +118,21 @@ public class CLI {
                 for (Method m: methods) {
                     if (m.getName().equals(name)) {
                         found = true;
-                        String[] params = new String[m.getParameterTypes().length];
-                        for (int i = 0; i < params.length; i++) {
-                            if (sc.hasNext()) {
-                                params[i] = sc.next();
-                            }
-                            else {
-                                System.out.println("wrong parameters");
-                                return;
-                            }
-                        }
-                        o = m.invoke(o, params);
+                        //System.out.println(sc.delimiter());
+                        //sc.useDelimiter("[\\p{javaWhitespace}\"]+");
+                        //System.out.println(sc.delimiter());
+                        //String[] params = new String[m.getParameterTypes().length];
+                        //for (int i = 0; i < params.length; i++) {
+                        //    if (sc.hasNext()) {
+                        //        params[i] = sc.next();
+                        //    }
+                        //    else {
+                        //        System.out.println("wrong parameters");
+                        //        return;
+                        //    }
+                        String params = sc.hasNextLine() ? sc.nextLine() : "";
+                        
+                        m.invoke(o, params);
                     }
                 }
                 if (found == false) {
@@ -113,7 +150,18 @@ public class CLI {
     }
     
     public static void main(String[] args) {
-        new CLI(new Controller());
+    	if (args.length == 0) {
+    		new CLI(new Controller());
+    	}
+    	else {
+    		String combinedArgs = "";
+    		for(String s : args) {
+    			combinedArgs += " " + s;
+    		}
+    		combinedArgs = combinedArgs.trim();
+    		System.out.println(combinedArgs);
+    		new CLI(new Controller(), combinedArgs);
+    	}
     }
     
 }
