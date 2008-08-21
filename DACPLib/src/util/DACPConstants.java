73ea04a6d81bbc4477c448eafaec41fd78a0c19f
oneilwill <oneilwill@c67ee986-0855-0410-825f-15918b819f62>
2008-08-21 22:22:15 +0000

    Added DACPConstants

---
+++ DACPLib/src/util/DACPConstants.java
@@ -0,0 +1,33 @@
+package util;
+
+import daap.DAAPConstants;
+
+public class DACPConstants extends DAAPConstants{
+	
+	public static final int cmst = parseInt("cmst");
+	public static final int mstt = parseInt("mstt");
+	public static final int cmsr = parseInt("cmsr");
+	public static final int caps = parseInt("caps");
+	public static final int cash = parseInt("cash");
+	public static final int carp = parseInt("carp");
+	public static final int caas = parseInt("caas");
+	public static final int caar = parseInt("caar");
+	public static final int canp = parseInt("canp");
+	public static final int cann = parseInt("cann");
+	public static final int cana = parseInt("cana");
+	public static final int canl = parseInt("canl");
+	public static final int cang = parseInt("cang");
+	public static final int asai = parseInt("asai");
+	public static final int cmmk = parseInt("cmmk");
+	public static final int cant = parseInt("cant");
+	public static final int cast = parseInt("cast");
+	
+	public static final int muty = parseInt("muty");
+	public static final int mtco = parseInt("mtco");
+	public static final int mrco = parseInt("mrco");
+	public static final int mlcl = parseInt("mlcl");
+	public static final int mlit = parseInt("mlit");
+	
+
+
+}
