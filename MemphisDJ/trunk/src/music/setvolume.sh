f1cc81af7951912daa1da7100b626cae4c47432e
Roma <Roma@c67ee986-0855-0410-825f-15918b819f62>
2008-08-21 00:00:47 +0000

    Corrected setVolume.sh

--- MemphisDJ/trunk/src/music/setvolume.sh
+++ MemphisDJ/trunk/src/music/setvolume.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ -x /bin/mixerctl ]; then
-	mixerctl -w outputs.mono=$1
-	mixerctl -w inputs.dac=$1
-elif [ -x /usr/bin/amixer ]; then
+mixerctl=$(which mixerctl)
+amixer=$(which amixer)
+if [ "x" != "$mixerctl" ]; then
+	$mixerctl -w outputs.mono=$1
+	$mixerctl -w inputs.dac=$1
+elif [ "x" != "$amixer" ]; then
 	vol=$(($1*100/255))
-	amixer -q set PCM $vol
+	$amixer -q set PCM $vol
 fi
