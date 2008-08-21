5ea0609c3efd5f584b0a6001416c561cd493eeca
Felix Shi <Felix Shi@c67ee986-0855-0410-825f-15918b819f62>
2008-08-21 03:28:53 +0000

    Volume script will now have linear control rather than quadratic.

--- MemphisDJ/trunk/src/music/setvolume.sh
+++ MemphisDJ/trunk/src/music/setvolume.sh
@@ -4,7 +4,7 @@ mixerctl=$(which mixerctl)
 amixer=$(which amixer)
 if [ "x" != "$mixerctl" ]; then
 	$mixerctl -w outputs.mono=$1
-	$mixerctl -w inputs.dac=$1
+	$mixerctl -w inputs.dac=255
 elif [ "x" != "$amixer" ]; then
 	vol=$(($1*100/255))
 	$amixer -q set PCM $vol
