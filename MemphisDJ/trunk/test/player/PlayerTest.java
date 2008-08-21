9ba5edb0bc0ba0d822a166f4c2fd53c334e94f8a
Roma <Roma@c67ee986-0855-0410-825f-15918b819f62>
2008-08-21 00:03:27 +0000

    Changed player test - bug fixing

--- MemphisDJ/trunk/test/player/PlayerTest.java
+++ MemphisDJ/trunk/test/player/PlayerTest.java
@@ -9,15 +9,22 @@ import daap.DAAPClient;
 public class PlayerTest {
 
 	public static void main(String args[]) {
-		String hostname = "fiebigs.mcs.vuw.ac.nz";
+		String hostname = "majoribanks.mcs.vuw.ac.nz";
 		int port = 3689;
 
 		try{
-			DAAPClient client = new DAAPClient(hostname, port);
-			testCompletePlay(client.getTrackList().get(1).getStream());
+			DAAPClient client = new DAAPClient(hostname,port);
+			testSkip(client.getStream(client.getTrackList().get(6)),client.getStream(client.getTrackList().get(8)));
 		}catch(IOException e){
+			System.out.println("FAILURE");
 			e.printStackTrace();
 		}
+//		try{
+//			DAAPClient client = new DAAPClient(hostname, port);
+//			testCompletePlay(client.getTrackList().get(1).getStream());
+//		}catch(IOException e){
+//			e.printStackTrace();
+//		}
 		
 //		testPlayer(PlayerTest.class.getResourceAsStream("music.mp3"));
 //
@@ -57,5 +64,25 @@ public class PlayerTest {
 		} catch (InterruptedException e) { e.printStackTrace();}
 
 		player.stop();
+	}
+	
+	public static void testSkip(InputStream in1, InputStream in2){
+		Player player = new Player();
+		player.setInputStream(in1);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+	
+			e.printStackTrace();
+		}
+		player.setInputStream(in2);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+	
+			e.printStackTrace();
+		}
+		player.stop();
+		
 	}
 }
