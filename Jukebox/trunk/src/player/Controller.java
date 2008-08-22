9637ec1a4ee431619afeb500781d70b5e2d72522
oneilwill <oneilwill@c67ee986-0855-0410-825f-15918b819f62>
2008-08-22 03:52:47 +0000

    Fixed some errors.

--- Jukebox/trunk/src/player/Controller.java
+++ Jukebox/trunk/src/player/Controller.java
@@ -1,7 +1,5 @@
 package player;
 
-
-
 import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -16,181 +14,205 @@ import playlist.Track;
 import controller.ControllerInterface;
 import daap.DAAPConstants;
 
-
 /**
  * 
  * @author coxdyla This class interfaces the User interfaces with the servers
  *         playlist TODO make it recieve and interpret DACP requests
  */
-public class Controller implements ControllerInterface{
-    
-    private final static boolean    DEBUG    = false;
-    
-    private DACPHeckler              dacp;
-    
-    public Controller() {
-        this.connect();
-    }
-    
-    private boolean connect() {
-        // TODO
-        // Assumes once connected, it is always connected.
-        
-        if (this.dacp == null) {
-            try {
-                this.dacp = new DACPHeckler("fiebigs", 3689);
-                return true;
-            }
-            catch (UnknownHostException e) {
-                if (DEBUG)
-                    e.printStackTrace();
-            }
-            catch (IOException e) {
-                if (DEBUG)
-                    e.printStackTrace();
-            }
-            return false;
-        }
-        return true;
-    }
-    
-    
+public class Controller implements ControllerInterface {
 
-    public boolean isValidController() {
-        return this.dacp != null;
-    }
-    
-    /* (non-Javadoc)
+	private final static boolean DEBUG = false;
+
+	private DACPHeckler dacp;
+
+	public Controller() {
+		this.connect();
+	}
+
+	private boolean connect() {
+		// TODO
+		// Assumes once connected, it is always connected.
+
+		if (this.dacp == null) {
+			try {
+				this.dacp = new DACPHeckler("cafe-bodega", 3689);
+				return true;
+			} catch (UnknownHostException e) {
+				if (DEBUG)
+					e.printStackTrace();
+			} catch (IOException e) {
+				if (DEBUG)
+					e.printStackTrace();
+			}
+			return false;
+		}
+		return true;
+	}
+
+	public boolean isValidController() {
+		return this.dacp != null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see player.ControllerInterface#pauseTrack()
 	 */
-    public void pauseTrack() {
-        if (this.connect())
-            this.dacp.pause();
-    }
-    
-    /* (non-Javadoc)
+	public void pauseTrack() {
+		if (this.connect())
+			this.dacp.pause();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see player.ControllerInterface#playTrack()
 	 */
-    public void playTrack() {
-        if (this.connect())
-            this.dacp.play();
-    }
-    
-    /* (non-Javadoc)
+	public void playTrack() {
+		if (this.connect())
+			this.dacp.play();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see player.ControllerInterface#getPlaylist()
 	 */
-    public List<Track> getPlaylist(){
-         return this.dacp.getTracks();
-    }
-    
-    /* (non-Javadoc)
+	public List<Track> getPlaylist() {
+		if (this.connect())
+			return this.dacp.getTracks();
+		return new ArrayList<Track>();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see player.ControllerInterface#changeVolume(int)
 	 */
-    public void changeVolume(int newVolume) {
-        if (newVolume < 0 || newVolume > 255)
-            throw new IllegalArgumentException("volume must be between 0-255");
-        if (this.connect())
-            this.dacp.setVolume(newVolume);
-    }
-    
-    /* (non-Javadoc)
+	public void changeVolume(int newVolume) {
+		if (newVolume < 0 || newVolume > 255)
+			throw new IllegalArgumentException("volume must be between 0-255");
+		if (this.connect())
+			this.dacp.setVolume(newVolume);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see player.ControllerInterface#skipTrack()
 	 */
-    public void skipTrack() {
-        this.dacp.skip();
-    }
-    
-    /* (non-Javadoc)
+	public void skipTrack() {
+		if (this.connect())
+			this.dacp.skip();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see player.ControllerInterface#getVolume()
 	 */
-    public int getVolume() {
-    	return this.dacp.getVolume();
-    }
+	public int getVolume() {
+		if (this.connect())
+			return this.dacp.getVolume();
+		return 0;
+	}
 
-    
-    
-    /*
-     * 
-     */
+	/*
+	 * 
+	 */
 	public void setPlaylist(List<Track> p) {
 		ArrayList<Track> tracks = new ArrayList<Track>();
-		for(Track t : tracks) tracks.add(t);
-		this.dacp.setTracks(tracks);
+		for (Track t : tracks)
+			tracks.add(t);
+		if (this.connect())
+			this.dacp.setTracks(tracks);
 	}
 
 	public void stop() {
-		this.dacp.stop();
+		if (this.connect())
+			this.dacp.stop();
 	}
 
-	
 	public void status() {
-		Track t = this.dacp.queryCurrentTrack();
-		if (t != null) //TODO print this better? What about paused/unpause?
-			System.out.println(t.toString());
-		
+		if (this.connect()) {
+			Track t = this.dacp.queryCurrentTrack();
+			if (t != null) // TODO print this better? What about
+				// paused/unpause?
+				System.out.println(t.toString());
+		}
+
 	}
-	
+
 	public void createPlaylistWithFilter(String type, String criteria) {
-		Map<Integer,String> filter=new HashMap<Integer, String>();
-		filter=fillFilter(type,criteria,filter);
-		this.dacp.setPlaylistWithFilter(filter);
+		if (this.connect()) {
+			Map<Integer, String> filter = new HashMap<Integer, String>();
+			filter = fillFilter(type, criteria, filter);
+			this.dacp.setPlaylistWithFilter(filter);
+		}
 	}
 
 	public void queryLibrary(String type, String crit) {
-		Map<Integer,String> filter=new HashMap<Integer, String>();
-		filter=fillFilter(type,crit,filter);
-		for (Track currentTrack : this.dacp.getPlaylistWithFilter(filter))
-			System.out.print(currentTrack);
+		if (this.connect()) {
+			Map<Integer, String> filter = new HashMap<Integer, String>();
+			filter = fillFilter(type, crit, filter);
+			for (Track currentTrack : this.dacp.getPlaylistWithFilter(filter))
+				System.out.print(currentTrack);
+		}
 	}
-	
-	//helper method
+
+	// helper method
 	/**
 	 * Returns a filtered playlist without replacing the current one.
+	 * 
 	 * @param type
 	 * @param criteria
 	 */
-	private Map<Integer,String> fillFilter(String type,String criteria,Map<Integer,String> playList){
+	private Map<Integer, String> fillFilter(String type, String criteria,
+			Map<Integer, String> playList) {
 		if (type.equalsIgnoreCase("artist"))
 			playList.put(DAAPConstants.ARTIST, criteria);
-		 else if (type.equalsIgnoreCase("album"))
+		else if (type.equalsIgnoreCase("album"))
 			playList.put(DAAPConstants.ALBUM, criteria);
-		
+
 		return playList;
 	}
 
 	public void queryRecentlyPlayed() {
-		List<Track> recent=this.dacp.getRecentlyPlayedTracks();
-		
-		System.out.println("Recently played Music\n-------------------");
-		while(!recent.isEmpty())
-			System.out.print(recent.remove(0));
+		if (this.connect()) {
+			List<Track> recent = this.dacp.getRecentlyPlayedTracks();
+
+			System.out.println("Recently played Music\n-------------------");
+			while (!recent.isEmpty())
+				System.out.print(recent.remove(0));
+		}
+
 	}
 
 	public void append(String type, String crit) {
-		//TODO
+		// TODO
 	}
 
 	public void displayLibrary() {
-		System.out.println("The Library Contents\n-------------------");
-		for(Track currentTrack : this.dacp.getLibrary())
-			System.out.print(currentTrack.toString());
-		
+		if (this.connect()) {
+			System.out.println("The Library Contents\n-------------------");
+			for (Track currentTrack : this.dacp.getLibrary())
+				System.out.print(currentTrack.toString());
+		}
+
 	}
-	
-    
-    public static void main(String[] args) {
-    	if (args.length == 0) {
-    		new CLI(new Controller());
-    	}
-    	else {
-    		String combinedArgs = "";
-    		for(String s : args) {
-    			combinedArgs += " " + s;
-    		}
-    		combinedArgs = combinedArgs.trim();
-    		System.out.println(combinedArgs);
-    		new CLI(new Controller(), combinedArgs);
-    	}
-    }
-    
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			new CLI(new Controller());
+		} else {
+			String combinedArgs = "";
+			for (String s : args) {
+				combinedArgs += " " + s;
+			}
+			combinedArgs = combinedArgs.trim();
+			System.out.println(combinedArgs);
+			new CLI(new Controller(), combinedArgs);
+		}
+	}
+
 }
