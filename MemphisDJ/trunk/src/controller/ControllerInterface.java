c159c3c79ec0ceb7b4a8733d81f012b74be2ae1f
stephen@sfnelson.org <stephen@sfnelson.org@c67ee986-0855-0410-825f-15918b819f62>
2008-09-02 01:11:01 +0000

    Changed classpath to remove static paths, misc refactoring and code cleanup to remove spurious warnings

---
+++ MemphisDJ/trunk/src/controller/ControllerInterface.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.util.List;
+
+import playlist.Track;
+
+
+
+public interface ControllerInterface {
+
+	/**
+	 * pauses the playing track
+	 * 
+	 */
+	public abstract void pauseTrack();
+
+	/**
+	 * plays the paused track
+	 */
+	public abstract void playTrack();
+	
+	public abstract void setPlaylist(List<Track> p);
+
+	public abstract List<Track> getPlaylist();
+
+	/**
+	 * change the volume to the stated value
+	 * 
+	 * @param newVolume
+	 *            int between 0 and 255
+	 * @throws IllegalArgumentException
+	 *             if newVolume is < 0 or > 255
+	 */
+	public abstract void changeVolume(int newVolume);
+
+	/**
+	 * skips to the next track
+	 * 
+	 */
+	
+	public abstract void skipTrack();
+
+	public abstract int getVolume();
+
+	public abstract void stop();
+	
+	public abstract void status();
+	
+	public abstract void createPlaylistWithFilter(String type, String criteria);
+
+	public abstract void queryRecentlyPlayed();
+
+	public abstract void queryLibrary(String type, String crit);
+	public abstract void displayLibrary();
+
+	public abstract void append(String type, String crit);
+	
+	
+
+}
