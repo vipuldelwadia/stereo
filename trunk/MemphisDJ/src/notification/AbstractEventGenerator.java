e03f7c70213531dd596e70b5a699395d03dc94bf
stephen@sfnelson.org <stephen@sfnelson.org@c67ee986-0855-0410-825f-15918b819f62>
2008-10-04 12:44:42 +0000

    Added enqueue functionality. Also made event generator synchorized to avoid concurrent modification while traversing. There may be a better solution: I'm not convinced that this is threadsafe.

--- trunk/MemphisDJ/src/notification/AbstractEventGenerator.java
+++ trunk/MemphisDJ/src/notification/AbstractEventGenerator.java
@@ -6,17 +6,17 @@ import java.util.Set;
 
 public class AbstractEventGenerator<T extends Listener> implements EventGenerator<T> {
 
-	public void registerListener(T listener) {
+	public synchronized void registerListener(T listener) {
 		listeners.add(listener);
 	}
 	
-	public void removeListener(T listener) {
+	public synchronized void removeListener(T listener) {
 		listeners.remove(listener);
 	}
 	
-	protected Iterable<T> listeners() {
+	protected synchronized Iterable<T> listeners() {
 		return new ArrayList<T>(listeners);
 	}
 	
-	private Set<T> listeners = new HashSet<T>();
+	private volatile Set<T> listeners = new HashSet<T>();
 }
