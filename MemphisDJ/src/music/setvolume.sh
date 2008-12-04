2abb8ab407fec3eeb22a61e858c274f00a1c5995
stephen@sfnelson.org <stephen@sfnelson.org@c67ee986-0855-0410-825f-15918b819f62>
2008-12-04 05:22:14 +0000

    Standardised on aumix for linux

--- MemphisDJ/src/music/setvolume.sh
+++ MemphisDJ/src/music/setvolume.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mixerctl=$(which mixerctl)
-amixer=$(which amixer)
+aumix=$(which aumix)
 if [ "x" != "x$mixerctl" ]; then
 	vol=$(($1*255/100))
 	$mixerctl -w outputs.mono=255
 	$mixerctl -w outputs.master=$vol
 #	$mixerctl -w inputs.dac=255
 elif [ "x" != "x$amixer" ]; then
-	$amixer -q set PCM $1
+	$aumix -w $1
 fi
