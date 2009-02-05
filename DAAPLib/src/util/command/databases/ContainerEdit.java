e4c99e33e53516ec012f72c30d4d9cbd99802f46
stephen@sfnelson.org <stephen@sfnelson.org@c67ee986-0855-0410-825f-15918b819f62>
2009-02-05 00:44:50 +0000

    Added functionality to remove items from a playlist

--- DAAPLib/src/util/command/databases/ContainerEdit.java
+++ DAAPLib/src/util/command/databases/ContainerEdit.java
@@ -41,6 +41,9 @@ public class ContainerEdit implements Command {
 		if (collection == null) {
 			throw new RuntimeException("collection not found (" + id + ")");
 		}
+		if (collection.editStatus() != Collection.EDITABLE) {
+			throw new RuntimeException("collection is not editable (" + id + ")");
+		}
 
 		String action = args.get("action");
 		String params = args.get("edit-params");
@@ -58,6 +61,26 @@ public class ContainerEdit implements Command {
 			System.out.println("found " + filtered.size() + " to add");
 
 			collection.add(filtered);
+		}
+		else if (action.equals("remove")) {
+			//action=remove&edit-params='dmap.containeritemid:17667'
+			Iterable<? extends Track> songs = collection.tracks();
+			List<? extends Track> filtered = null;
+
+			if (params != null) {
+				Filter f = QueryParser.parse(params);
+				System.out.println(f);
+				filtered = ApplyFilter.filter(f, songs);
+			}
+
+			System.out.println("found " + filtered.size() + " to remove");
+
+			collection.remove(filtered);
+		}
+		else if (action.equals("move")) {
+			//action=move&edit-params='edit-param.move-pair:17978,17980'
+			
+			throw new RuntimeException("action: move is not yet implemented");
 		}
 		else {
 			throw new RuntimeException("unknown action: " + action);
